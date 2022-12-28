f2c40447d0ffb90b80c2ffd44bd407bdbdd1fb7a
ziontee113 <ziontee113@gmail.com>
2022-12-28 13:54:35 +0700

    Add paragraph-jumping <A-hjkl> mappings

---
+++ lua/mappings/paragraph-jumping.lua
@@ -0,0 +1,25 @@
+local function handle_top_bottom_line_jump(jumpCmd)
+    vim.cmd(jumpCmd)
+    local curLine = vim.api.nvim_win_get_cursor(0)[1]
+    if curLine == 2 then
+        vim.cmd("normal! k")
+    elseif curLine + 1 == vim.api.nvim_buf_line_count(0) then
+        if jumpCmd == "keepjumps normal! j}k" then
+            vim.cmd("normal! j")
+        end
+    end
+end
+
+vim.keymap.set({ "n", "x", "o" }, "<A-j>", function()
+    handle_top_bottom_line_jump("keepjumps normal! j}k")
+end)
+vim.keymap.set({ "n", "x", "o" }, "<A-k>", function()
+    handle_top_bottom_line_jump("keepjumps normal! k{j")
+end)
+
+vim.keymap.set({ "n", "x", "o" }, "<A-h>", function()
+    vim.cmd("keepjumps normal! {")
+end)
+vim.keymap.set({ "n", "x", "o" }, "<A-l>", function()
+    vim.cmd("keepjumps normal! }")
+end)

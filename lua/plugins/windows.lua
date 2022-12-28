84012b22a2c55eab8fbdf5919f25dc3822c904a7
ziontee113 <ziontee113@gmail.com>
2022-12-28 08:19:56 +0700

    Add windows.nvim

---
+++ lua/plugins/windows.lua
@@ -0,0 +1,19 @@
+return {
+	"anuvyklack/windows.nvim",
+	dependencies = {
+		"anuvyklack/middleclass",
+		"anuvyklack/animation.nvim",
+	},
+	config = function()
+		vim.o.winwidth = 10
+		vim.o.winminwidth = 10
+		vim.o.equalalways = false
+
+		require("windows").setup()
+
+		vim.keymap.set("n", "<C-z>", ":WindowsMaximize<cr>")
+		vim.keymap.set("n", "<C-w>_", ":WindowsMaximizeVertically<cr>")
+		vim.keymap.set("n", "<C-w>|", ":WindowsMaximizeHorizontally<cr>")
+		vim.keymap.set("n", "<C-w>=", ":WindowsEqualize<cr>")
+	end,
+}
--- lua/settings/colors.lua
+++ lua/settings/colors.lua
@@ -1,3 +1,6 @@
 vim.cmd([[
     hi SignColumn guibg=NONE
+    hi VertSplit guibg=NONE
+    hi StatusLine guifg=#282828 guibg=#665c54
+    hi StatusLineNC guifg=#282828 guibg=#665c54
 ]])

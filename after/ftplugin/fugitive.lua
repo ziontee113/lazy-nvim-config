9b84ac8d1089cfc1bada5825cf172bd3deb081d4
ziontee113 <ziontee113@gmail.com>
2022-12-28 13:17:25 +0700

    Add stage/unstage all fugitive mappings

--- after/ftplugin/fugitive.lua
+++ after/ftplugin/fugitive.lua
@@ -1,1 +1,6 @@
 vim.keymap.set("n", "<C-p>", ":G push", { buffer = 0 })
+vim.keymap.set("n", "q", ":q<cr>", { buffer = 0 })
+
+-- Staging Remaps
+vim.keymap.set("n", "S", "5ggs", { buffer = 0, remap = true })
+vim.keymap.set("n", "U", "5ggu", { buffer = 0, remap = true })

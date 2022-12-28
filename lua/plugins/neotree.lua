1d1aa089ad166a2fff88fbf05f3e092306cfb618
ziontee113 <ziontee113@gmail.com>
2022-12-28 13:36:30 +0700

    Add h and l to open/toggle mappings for neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,24 +1,29 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v2.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	keys = {
-		{ "\\", "<cmd>NeoTreeFloatToggle<cr>", desc = "NeoTree" },
-	},
-	config = function()
-		require("neo-tree").setup({
-			window = {
-				mappings = {
-					["v"] = "open_vsplit",
-					["<C-v>"] = "open_vsplit",
-					["z"] = "close_all_nodes",
-					["Z"] = "expand_all_nodes",
-				},
-			},
-		})
-	end,
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v2.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+    },
+    keys = {
+        { "\\", "<cmd>NeoTreeFloatToggle<cr>", desc = "NeoTree" },
+    },
+    config = function()
+        require("neo-tree").setup({
+            window = {
+                mappings = {
+                    ["h"] = {
+                        "toggle_node",
+                        nowait = true,
+                    },
+                    ["l"] = "open",
+                    ["v"] = "open_vsplit",
+                    ["<C-v>"] = "open_vsplit",
+                    ["z"] = "close_all_nodes",
+                    ["Z"] = "expand_all_nodes",
+                },
+            },
+        })
+    end,
 }

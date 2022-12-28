b3225aff7d9f24b909b1ec17693b9dc476f969e3
ziontee113 <ziontee113@gmail.com>
2022-12-28 18:57:36 +0700

    Format plugins/telescope with stylua

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,39 +1,43 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	branch = "0.1.x",
-	dependencies = { { "nvim-lua/plenary.nvim" } },
-	cmd = "Telescope",
-	keys = {
-		{ "<C-p>", "<cmd>Telescope find_files<cr>", desc = "Telescope find_files" },
-		{ "<C-g>", "<cmd>Telescope live_grep<cr>", desc = "Telescope live_grep" },
-		{ "<C-f>", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Telescope find_files" },
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = { { "nvim-lua/plenary.nvim" } },
+    cmd = "Telescope",
+    keys = {
+        { "<C-p>", "<cmd>Telescope find_files<cr>", desc = "Telescope find_files" },
+        { "<C-g>", "<cmd>Telescope live_grep<cr>", desc = "Telescope live_grep" },
+        { "<C-f>", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Telescope find_files" },
 
-		{ "<leader>f", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Telescope find_files" },
-		{ "<leader>e", "<cmd>Telescope find_files<cr>", desc = "Telescope find_files" },
-		{ "<leader>g", "<cmd>Telescope grep_string<cr>", desc = "Telescope live_grep" },
-		{ "<leader>h", "<cmd>Telescope highlights<cr>", desc = "Telescope live_grep" },
-	},
-	config = function()
-		local actions = require("telescope.actions")
-		require("telescope").setup({
-			defaults = {
-				dynamic_preview_title = true,
-				mappings = {
-					i = {
-						["<C-j>"] = actions.move_selection_next,
-						["<C-k>"] = actions.move_selection_previous,
-						["<C-l>"] = actions.select_default,
-					},
-				},
-			},
-			pickers = {
-				find_files = {
-					find_command = { "rg", "--files", "--hidden", "-g", "!.git" },
-				},
-				lsp_dynamic_workspace_symbols = {
-					file_ignore_patterns = { "node_modules/" },
-				},
-			},
-		})
-	end,
+        {
+            "<leader>f",
+            "<cmd>Telescope current_buffer_fuzzy_find<cr>",
+            desc = "Telescope find_files",
+        },
+        -- { "<leader>e", "<cmd>Telescope find_files<cr>", desc = "Telescope find_files" },
+        { "<leader>g", "<cmd>Telescope grep_string<cr>", desc = "Telescope live_grep" },
+        { "<leader>h", "<cmd>Telescope highlights<cr>", desc = "Telescope live_grep" },
+    },
+    config = function()
+        local actions = require("telescope.actions")
+        require("telescope").setup({
+            defaults = {
+                dynamic_preview_title = true,
+                mappings = {
+                    i = {
+                        ["<C-j>"] = actions.move_selection_next,
+                        ["<C-k>"] = actions.move_selection_previous,
+                        ["<C-l>"] = actions.select_default,
+                    },
+                },
+            },
+            pickers = {
+                find_files = {
+                    find_command = { "rg", "--files", "--hidden", "-g", "!.git" },
+                },
+                lsp_dynamic_workspace_symbols = {
+                    file_ignore_patterns = { "node_modules/" },
+                },
+            },
+        })
+    end,
 }

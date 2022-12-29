5c0bcf816214a2bd5f2ea25a7ef669dc1082924c
ziontee113 <ziontee113@gmail.com>
2022-12-29 16:20:52 +0700

    Add lazygit mappings

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,6 +2,8 @@ return {
     "akinsho/toggleterm.nvim",
     keys = {
         { [[<C-\>]] },
+        { [[<C-3>]] },
+        { [[<C-4>]] },
     },
     config = function()
         require("toggleterm").setup({
@@ -13,5 +15,40 @@ return {
             persist_size = true,
             direction = "float",
         })
+
+        local Terminal = require("toggleterm.terminal").Terminal
+        local lazygit_normal
+        local lazygit_big
+
+        local function shutdown_then_toggle_toggleterm_terminal(
+            terminal_variable,
+            terminal_create_function
+        )
+            terminal_variable = terminal_create_function()
+            terminal_variable:toggle()
+        end
+
+        local function create_lazygit_normal()
+            return Terminal:new({
+                cmd = "EDITOR=nvim lazygit",
+                hidden = true,
+            })
+        end
+
+        local function create_lazygit_big()
+            return Terminal:new({
+                cmd = "EDITOR=nvim lazygit",
+                hidden = true,
+                float_opts = { width = 1920, border = "none", height = 1080 },
+            })
+        end
+
+        vim.keymap.set("n", "<C-3>", function() -- lazygit_normal
+            shutdown_then_toggle_toggleterm_terminal(lazygit_normal, create_lazygit_normal)
+        end, { silent = true })
+
+        vim.keymap.set("n", "<C-4>", function() -- lazygit_big
+            shutdown_then_toggle_toggleterm_terminal(lazygit_big, create_lazygit_big)
+        end, { silent = true })
     end,
 }

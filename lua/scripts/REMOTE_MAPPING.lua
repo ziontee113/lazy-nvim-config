adbadb55d46df4994cb410b3922ba9c613455246
ziontee113 <ziontee113@gmail.com>
2022-12-31 12:04:44 +0700

    Use <Plug> instead of calling :lua

--- lua/scripts/REMOTE_MAPPING.lua
+++ lua/scripts/REMOTE_MAPPING.lua
@@ -1,30 +1,23 @@
-FEED = function(mapping, mode)
+---@diagnostic disable: unused-local, unused-function
+
+local feed = function(mapping, mode)
     local key = vim.api.nvim_replace_termcodes(mapping, true, false, true)
     vim.api.nvim_feedkeys(key, mode or "m", false)
 end
 
-MY_REMOTE_MAPPINGS = {
-    ["L1 D Down, R1 J Down"] = ":vs<cr>",
-    ["L1 D Down, R1 K Down"] = ":q<cr>",
-    ["L1 D Down, R1 H Down"] = function()
-        FEED("4k")
-    end,
-    ["L1 D Down, R1 L Down"] = function()
-        FEED("4j")
-    end,
-    ["L1 D Down, R1 O Down"] = function()
-        print("hello world")
-    end,
-}
-
-ACTIVATE_REMOTE_MAPPING = function(mapping)
-    if MY_REMOTE_MAPPINGS[mapping] then
-        if type(MY_REMOTE_MAPPINGS[mapping]) == "string" then
-            FEED(MY_REMOTE_MAPPINGS[mapping])
-        else
-            MY_REMOTE_MAPPINGS[mapping]()
-        end
-    end
-
-    print("")
+local type = function(mapping)
+    local key = vim.api.nvim_replace_termcodes(mapping, true, false, true)
+    vim.api.nvim_feedkeys(key, "Rns", false)
 end
+
+vim.keymap.set("n", "<Plug>L1 D Down, R1 J Down", function()
+    print("hello world!")
+end, {})
+
+vim.keymap.set("i", "<Plug>L1 D Down, R1 J Down", function()
+    type("shut down")
+end, {})
+
+vim.keymap.set("i", "<Plug>L1 D Down, R1 K Down", function()
+    type("typa girl")
+end, {})

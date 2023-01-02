75686e78964e1775735e12efe6ff8e0566342339
ziontee113 <ziontee113@gmail.com>
2023-01-02 17:09:47 +0700

    Use mini.comment

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -3,5 +3,13 @@ return {
     event = "VeryLazy",
     config = function()
         require("mini.ai").setup()
+        require("mini.comment").setup({
+            mappings = {
+                textobject = "ic",
+            },
+        })
+
+        vim.keymap.set("n", "", "gcc", { remap = true })
+        vim.keymap.set("x", "", "gc", { remap = true })
     end,
 }

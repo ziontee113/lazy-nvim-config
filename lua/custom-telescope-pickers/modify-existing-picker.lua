da24d855f4fba798c59c3d908db335d83ae23620
ziontee113 <ziontee113@gmail.com>
2023-01-26 20:49:41 +0700

    Add modify existing picker experiment

--- lua/custom-telescope-pickers/modify-existing-picker.lua
+++ lua/custom-telescope-pickers/modify-existing-picker.lua
@@ -1,14 +1,6 @@
-local pickers = require("telescope.pickers")
-local finders = require("telescope.finders")
-local sorters = require("telescope.sorters")
-
 local actions = require("telescope.actions")
 local action_state = require("telescope.actions.state")
-
-local themes = require("telescope.themes")
 local builtin = require("telescope.builtin")
-
-local M = {}
 
 local opts = {
     prompt_title = "My Dotfiles",

3d7406360a67eac66b32a5441159634e0ff8c35d
ziontee113 <ziontee113@gmail.com>
2023-01-26 20:11:58 +0700

    Wrap simple picker in function then call it

--- lua/custom-telescope-pickers/init.lua
+++ lua/custom-telescope-pickers/init.lua
@@ -6,37 +6,42 @@ local themes = require("telescope.themes")
 local actions = require("telescope.actions")
 local action_state = require("telescope.actions.state")
 
-local my_table = {
-    "yellow",
-    "orange",
-    "red",
-}
+local first_picker = function()
+    local my_table = {
+        "yellow",
+        "orange",
+        "red",
+    }
 
-local layout = {
-    layout_config = {
-        height = 15,
-        width = 60,
-    },
-}
+    local layout = {
+        layout_config = {
+            height = 15,
+            width = 60,
+        },
+    }
 
-local my_func = function()
-    local selected = action_state.get_selected_entry()
+    local my_func = function(prompt_bufnr)
+        local selected_entry = action_state.get_selected_entry()
 
-    N(selected[1])
+        N(selected_entry[1])
 
-    actions.close()
+        actions.close(prompt_bufnr)
+    end
+
+    local opts = {
+        finder = finders.new_table(my_table),
+        sorter = sorters.get_generic_fuzzy_sorter({}),
+
+        attach_mappings = function(_, map)
+            map("i", "<CR>", my_func)
+            return true
+        end,
+    }
+
+    local picker = pickers.new(layout, opts)
+
+    return picker
 end
 
-local opts = {
-    finder = finders.new_table(my_table),
-    sorter = sorters.get_generic_fuzzy_sorter({}),
-
-    attach_mappings = function(_, map)
-        map("i", "<CR>", my_func)
-        return true
-    end,
-}
-
-local picker = pickers.new(layout, opts)
-
+local picker = first_picker()
 picker:find()

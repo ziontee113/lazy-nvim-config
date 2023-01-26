158de27bd942b967f18f7295889e9579b0b022ed
ziontee113 <ziontee113@gmail.com>
2023-01-26 20:22:22 +0700

    Use M, re-order pickers

--- lua/custom-telescope-pickers/init.lua
+++ lua/custom-telescope-pickers/init.lua
@@ -6,7 +6,53 @@ local themes = require("telescope.themes")
 local actions = require("telescope.actions")
 local action_state = require("telescope.actions.state")
 
-local second_picker = function(data_tbl)
+local M = {}
+
+M.first_picker = function()
+    local my_table = {
+        "yellow",
+        "orange",
+        "red",
+    }
+
+    local layout = {
+        layout_config = {
+            height = 15,
+            width = 60,
+        },
+    }
+
+    local first_picker_func = function(prompt_bufnr)
+        local selected_entry = action_state.get_selected_entry()
+
+        selected_entry = selected_entry[1]
+        local new_tbl = {}
+        for i = 1, 10, 1 do
+            table.insert(new_tbl, selected_entry .. " - " .. tostring(i))
+        end
+
+        local new_picker = M.second_picker(new_tbl)
+        new_picker:find()
+
+        -- actions.close(prompt_bufnr)
+    end
+
+    local opts = {
+        finder = finders.new_table(my_table),
+        sorter = sorters.get_generic_fuzzy_sorter({}),
+
+        attach_mappings = function(_, map)
+            map("i", "<CR>", first_picker_func)
+            return true
+        end,
+    }
+
+    local picker = pickers.new(layout, opts)
+
+    return picker
+end
+
+M.second_picker = function(data_tbl)
     local layout = {
         layout_config = {
             height = 15,
@@ -35,49 +81,5 @@ local second_picker = function(data_tbl)
     return picker
 end
 
-local first_picker = function()
-    local my_table = {
-        "yellow",
-        "orange",
-        "red",
-    }
-
-    local layout = {
-        layout_config = {
-            height = 15,
-            width = 60,
-        },
-    }
-
-    local first_picker_func = function(prompt_bufnr)
-        local selected_entry = action_state.get_selected_entry()
-
-        selected_entry = selected_entry[1]
-        local new_tbl = {}
-        for i = 1, 10, 1 do
-            table.insert(new_tbl, selected_entry .. " - " .. tostring(i))
-        end
-
-        local new_picker = second_picker(new_tbl)
-        new_picker:find()
-
-        -- actions.close(prompt_bufnr)
-    end
-
-    local opts = {
-        finder = finders.new_table(my_table),
-        sorter = sorters.get_generic_fuzzy_sorter({}),
-
-        attach_mappings = function(_, map)
-            map("i", "<CR>", first_picker_func)
-            return true
-        end,
-    }
-
-    local picker = pickers.new(layout, opts)
-
-    return picker
-end
-
-local picker = first_picker()
+local picker = M.first_picker()
 picker:find()

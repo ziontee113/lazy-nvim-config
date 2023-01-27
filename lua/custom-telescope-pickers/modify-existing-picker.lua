baf4af30c9a6bfe1165a9c6634b9c54836695393
ziontee113 <ziontee113@gmail.com>
2023-01-27 11:57:44 +0700

    Edit builtin find_files to print selected results

--- lua/custom-telescope-pickers/modify-existing-picker.lua
+++ lua/custom-telescope-pickers/modify-existing-picker.lua
@@ -2,9 +2,29 @@ local actions = require("telescope.actions")
 local action_state = require("telescope.actions.state")
 local builtin = require("telescope.builtin")
 
+local picker_func = function(prompt_bufnr)
+    local picker = action_state.get_current_picker(prompt_bufnr)
+
+    local selected_entries = {}
+    for _, entry in ipairs(picker:get_multi_selection()) do
+        table.insert(selected_entries, entry[1])
+    end
+
+    if #selected_entries == 0 then
+        table.insert(selected_entries, action_state.get_selected_entry()[1])
+    end
+
+    N(selected_entries)
+end
+
 local opts = {
     prompt_title = "My Dotfiles",
     search_dirs = { "~/dotfiles/" },
     shorten_path = true,
+
+    attach_mappings = function(_, map)
+        map("i", "<CR>", picker_func)
+        return true
+    end,
 }
 builtin.find_files(opts)

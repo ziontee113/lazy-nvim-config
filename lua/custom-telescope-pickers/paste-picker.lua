3b1810a8b462a52fe42298dd51166b5c97580c16
ziontee113 <ziontee113@gmail.com>
2023-01-27 13:58:32 +0700

    Add paste-picker for Telescope

---
+++ lua/custom-telescope-pickers/paste-picker.lua
@@ -0,0 +1,33 @@
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
+local builtin = require("telescope.builtin")
+
+local target_bufnr = 0 -- could've been nicer, but this works
+
+local picker_func = function(prompt_bufnr)
+    local path = action_state.get_selected_entry()[1]
+    local lines = vim.fn.readfile(path)
+
+    vim.api.nvim_buf_set_lines(target_bufnr, 0, -1, false, lines)
+
+    actions.close(prompt_bufnr)
+end
+
+local opts = {
+    prompt_title = "My Dotfiles",
+    search_dirs = { "~/dotfiles/" },
+    shorten_path = true,
+
+    attach_mappings = function(_, map)
+        map("i", "<CR>", picker_func)
+        return true
+    end,
+}
+
+vim.keymap.set("n", "<leader>x", function()
+    target_bufnr = vim.api.nvim_get_current_buf()
+
+    builtin.find_files(opts)
+end, {})
+
+-- {{{nvim-execute-on-save}}}
